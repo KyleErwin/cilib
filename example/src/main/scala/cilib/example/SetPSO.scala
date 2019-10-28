e610f1db68d082ec09364999f94420367c23444d
Kyle <kyle.erwin24@gmail.com>
2019-10-28 22:37:49 +0200

    WIP

--- example/src/main/scala/cilib/example/SetPSO.scala
+++ example/src/main/scala/cilib/example/SetPSO.scala
@@ -4,12 +4,59 @@ package example
 import scalaz.Scalaz._
 import scalaz._
 import scalaz.effect._
+import scalaz.effect.IO.putStrLn
 
-object FileOutput extends SafeApp {
+import eu.timepit.refined._
+import eu.timepit.refined.api._
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric.Positive
 
-  val x: String = "45"
+object SetPSO extends SafeApp {
 
-  def x(xs: List[Double]): Double =
-    xs.suml
+  sealed abstract class Operation {
+    def run(x: Double, y: Double): Double =
+      this match {
+        case Minus() => x - y
+        case Plus()  => x + y
+      }
+  }
+  final case class Minus() extends Operation
+  final case class Plus() extends Operation
+
+  refineV[Positive](52)
+  final case class Pair(operation: Operation, value: Int)
+
+  def addition(v1: List[Pair], v2: List[Pair]): List[Pair] =
+    (v1 ::: v2).distinct
+
+  def difference(p1: List[Int], p2: List[Int]): List[Pair] = {
+    val xs = p1.foldLeft(List[Pair]()) {
+      case (a, c) => if (!p2.contains(c)) Pair(Plus(), c) :: a else a
+    }
+    val ugh = p2.foldLeft(List[Pair]()) {
+      case (a, c) => if (!p1.contains(c)) Pair(Minus(), c) :: a else a
+    }
+    xs ::: ugh
+  }
+
+  def multiplcationByScalar[A](velocity: List[A]): RVar[List[A]] =
+    Dist.stdUniform
+      .flatMap { n =>
+        refineV[Positive]((n * velocity.size).toInt) match {
+          case Right(refined) =>
+            RVar.choices(refined, velocity).map { result =>
+              result.getOrElse(velocity)
+            }
+          case Left(_) => RVar.pure(velocity)
+        }
+      }
+
+  val pos1 = List(1, 2, 3)
+  val pos2 = List(1, 4, 5)
+  override val runc: IO[Unit] =
+    for {
+      _ <- putStrLn(difference(pos1, pos2).toString())
+      _ <- putStrLn("Complete.")
+    } yield ()
 
 }
